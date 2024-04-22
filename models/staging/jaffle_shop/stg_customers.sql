7c59f4b13d54db432011f2661193b56a38272bf5
jsphszot <jsphszot@gmail.com>
2024-04-22 13:53:37 +0200

    editing stg and dim, materializing from yml

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,2 +1,10 @@
 
-select * from `dbt-tutorial.jaffle_shop.customers`
+with customers as (
+    select 
+    id as customer_id
+    , first_name
+    , last_name
+    from `dbt-tutorial.jaffle_shop.customers`
+)
+
+select * from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,2 +1,13 @@
 
-select * from `dbt-tutorial.jaffle_shop.orders`
+with
+
+orders as (
+    select 
+    id as order_id
+    , user_id as customer_id
+    , order_date
+    , status
+    from `dbt-tutorial.jaffle_shop.orders`
+)
+
+select * from orders
